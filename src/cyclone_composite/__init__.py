0e69360903f64b308190fe2a9e91d13bd74779a8
vasanchez16 <vsanchez@andrew.cmu.edu>
2025-06-05 12:50:33 -0400

    make get centers func to aggregate all helper funcs

--- src/cyclone_composite/__init__.py
+++ src/cyclone_composite/__init__.py
@@ -8,5 +8,8 @@ from .locate_centers import *
 __version__ = '0.1.0'
 
 __all__ = [
-    '__version__'
+    '__version__',
+    rate_of_change.__all__,
+    utils.__all__,
+    locate_centers.__all__
 ]
---
+++ src/cyclone_composite/get_centers.py
@@ -0,0 +1,49 @@
+from .locate_centers import find_candidate_points, find_centers
+from .rate_of_change import get_dx, get_dy, first_order_rate_of_change, get_roc_on_full_grid
+from .utils import get_coord_midpoints, haversine
+
+import numpy as np
+
+def get_centers(
+    press_data,
+    latitude_arr,
+    longitude_arr,
+    first_order_max = 4E-5,
+    second_order_min = 9E-5,
+    pressure_max = 1015
+):
+
+    dx = get_dx(latitude_arr, longitude_arr)
+    dy = get_dy(latitude_arr)
+
+    lat_midpoints, lon_midpoints = get_coord_midpoints(latitude_arr, longitude_arr)
+
+    x_1roc, y_1roc, x_2roc, y_2roc = get_roc_on_full_grid(latitude_arr, longitude_arr, lat_midpoints, lon_midpoints, dx, dy, press_data)
+
+    limits = (first_order_max, second_order_min)
+    center_candidate_points = find_candidate_points(x_1roc, y_1roc, x_2roc, y_2roc, limits)
+
+    lons_grid, lats_grid = np.meshgrid(longitude_arr, latitude_arr)
+    lats_grid = lats_grid.flatten()
+    lons_grid = lons_grid.flatten()
+
+    cyclone_centers_flat = center_candidate_points.flatten()
+
+    candidate_points_lats = lats_grid[cyclone_centers_flat]
+    candidate_points_lons = lons_grid[cyclone_centers_flat]
+
+    centers = []
+
+    for lat, lon in zip(candidate_points_lats, candidate_points_lons):
+        temp_msl = press_data.copy()
+
+        dists = haversine((lat, lon), (lats_grid, lons_grid))
+        dists = np.reshape(dists, (latitude_arr.shape[0], longitude_arr.shape[0]))
+        bool_arr = (temp_msl < pressure_max) & (dists < 2000) & (center_candidate_points)
+        temp_msl[~bool_arr] = np.nan
+
+        centers.append(temp_msl == np.nanmin(temp_msl))
+    centers = np.array(centers)    
+    centers = np.any(centers, axis=0)
+
+    return centers
